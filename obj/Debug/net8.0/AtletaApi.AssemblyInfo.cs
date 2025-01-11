460081ab5cdf8565b2c46995c730a2d82c6601dc
arthur-mendonca <arthu91@gmail.com>
2025-01-11 09:28:57 -0300

    Instalar Sqlite

--- obj/Debug/net8.0/AtletaApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/AtletaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtletaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f0f3d56e80299ecc72da7236ff65ed3540ab8c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+033bd13abcb6c22f3c2b47ac787569fbce602ae0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtletaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtletaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
